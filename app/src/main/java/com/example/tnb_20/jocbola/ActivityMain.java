60d0970f83c122c1c70b4940b9b17216f977ff17
DarioDelCs <dariodelc@hotmail.com>
2018-12-10 19:42:19 +0100

    primer tag

---
+++ app/src/main/java/com/example/tnb_20/jocbola/ActivityMain.java
@@ -0,0 +1,108 @@
+package com.example.tnb_20.jocbola;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+
+public class ActivityMain extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorMgr;
+    Sensor sensor;
+    ImageView img;
+
+    float velocitat = 2.0f;
+    float iniciX, iniciY;
+
+    int statusBar, width, height;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // La bola
+        img = (ImageView) findViewById(R.id.imageView);
+
+        // Obtenim les dimensions de la pantalla
+        DisplayMetrics display = this.getBaseContext().getResources().getDisplayMetrics();
+        width = display.widthPixels;
+        height = display.heightPixels;
+
+        // Mida de l'statusBar per calcular l'alçada de l'aplicació
+        statusBar = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android"));
+
+        // Inicialitzem a 0 les variables per controlar les pulsacions tàctils
+        iniciX = iniciY = 0;
+
+        // Inicialitzem el sensor de l'acceleròmetre
+        sensorMgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMgr.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMgr.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorMgr.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            moveBall(sensorEvent.values[1], sensorEvent.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor s, int i) {
+    }
+
+    private void moveBall(float x, float y) {
+        // Coordenada x
+        float novaPosicioX = img.getX() + x * velocitat;
+        if (x > 0) {// Si el moviment és cap a la dreta
+            // Comprovem que no surti de les dimensions de la pantalla en assignar la nova posició
+            if (novaPosicioX + img.getWidth() <= width) {
+                img.setX(novaPosicioX);
+            } else {// Si en surt, establim la posició màxima en horitzontal perquè es pugui veure la imatge.
+                img.setX(width - img.getWidth());
+            }
+        } else {// Fem el mateix pel moviment cap a l'esquerra
+            if (novaPosicioX >= 0) {
+                img.setX(novaPosicioX);
+            } else{
+                img.setX(0);
+            }
+        }
+
+        // Coordenada y
+        float novaPosicioY = img.getY() + y * velocitat;
+        // El concepte és el mateix que a la X
+        if (y > 0) {
+            if (novaPosicioY + img.getHeight() + statusBar <= height) {
+                img.setY(novaPosicioY);
+            } else {
+                img.setY(height - img.getHeight() - statusBar);
+            }
+        }else {
+            if (novaPosicioY >= 0) {
+                img.setY(novaPosicioY);
+            } else {
+                img.setY(0);
+            }
+        }
+    }
+}
